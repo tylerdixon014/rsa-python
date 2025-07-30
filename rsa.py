366d05d205e0f56aa0b5fa6aace2647c6f1f75d4
tylerdixon014 <tdixon@binghamton.edu>
2025-07-30 16:38:38 -0700

    corersa
    wrote the core rsa implementation; public and private keys, encryption and decryption

--- rsa.py
+++ rsa.py
@@ -1,12 +1,14 @@
 import random
 import math
 
+e = 65537
+
 # prime generator function (sieve of eratosthenes)
-def eratosthenes():
+def sieve(limit):
     D = {}
     q = 2
 
-    while True:
+    while q <= limit:
         if q not in D:
             yield q
             D[q * q] = [q]
@@ -17,18 +19,57 @@ def eratosthenes():
         
         q += 1
 
-gen_primes = eratosthenes()
+def random_prime(bit_length):
+    lower_bound = 2**(bit_length - 1)
+    upper_bound = 2**(bit_length)
+
+    primes = sieve(upper_bound)
+    valid_primes = [p for p in primes if lower_bound <= p]
+
+    if not valid_primes:
+        print("There are no valid primes of the desired bit length")
+
+    return random.choice(valid_primes)
 
 # generate keys 
-    # public: (e,n)
-    # private: (d,n)
+
+def extended_gcd(a,b):  # extended euclidean algorithm
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = extended_gcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+    
+def modinv(a, m): # modular inverse function
+    g, x, y = extended_gcd(a,m)
+    if g!= 1:
+        raise Exception("Modular inverse DNE")
+    else:
+        return x % m
+
+def generate_keys(bit_length):
+    p = random_prime(bit_length)
+    q = random_prime(bit_length)
+    while p == q:
+        q = random_prime(bit_length)
+
+    n = p * q
+    totient = (p - 1) * (q - 1)
+    d = modinv(e, totient)
+
+    public_key = (e, n)
+    private_key = (d, n)
+
+    return public_key, private_key
 
 # encryption / decryption
+def encrypt(message, public_key):
+    e, n = public_key
+    ciphertext = pow(message, e, n)
+    return ciphertext
 
-# main
+def decrpyt(ciphertext, private_key):
+    d, n = private_key
+    message_decoded = pow(ciphertext, d, n)
+    return message_decoded
 
-for i in gen_primes:
-    if i <= 1000:
-        print(i)
-    else:
-        break
